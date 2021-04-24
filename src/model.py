9549a1242b3ee0684fdd626b912654d36535da64
subodhsandiego <83114122+subodhsandiego@users.noreply.github.com>
2021-04-24 15:48:22 -0700

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,28 +1,33 @@
-from sklearn.linear_model import SGDClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn import tree
 import numpy as np
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+
 
 
 class BaselineModel:
     def __init__(self, model_file_path):
         self.model_file_path = model_file_path
 
+    def vectorize_sequences(self, sequence_array):
+        vectorize_on_length = np.vectorize(len)
+        return np.reshape(vectorize_on_length(sequence_array), (-1, 1))
 
-    def train(self,X, df_train):
-       
+    def train(self, df_train):
+        X = self.vectorize_sequences(df_train['sequence'].to_numpy())
+        y = df_train['mean_growth_PH'].to_numpy()
 
         model = SGDClassifier()
-        model.fit(X, df_train)
+        model.fit(X, y)
 
         with open(self.model_file_path, 'wb') as model_file:
             pickle.dump(model, model_file)
 
     def predict(self, df_test):
         with open(self.model_file_path, 'rb') as model_file:
-            model: tree.DecisionTreeRegressor = pickle.load(model_file)
-                
-        tfidf = tfidfvectorizer.transform(df_test.sequence)
-        
-        return model.predict(tfidf)
+            model: SGDClassifier() = pickle.load(model_file)
+        #tfidfvectorizer = pickle.load(open('src/tfidf.pickle','rb'))    
+        #tfidf = tfidfvectorizer.fit_transform(df_test)  
+
+        return model.predict(df_test)
